9bef7d8c5de76c5fa1cea27b543a4fe1c0b261b0
kalpana <kalpanaironbanda@gmail.com>
2023-05-11 13:16:29 +0530

    modified the dependencies file

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install python3 -y
 yum install python3-pip -y
-pip install flask -y
+pip install flask 
 dnf install mariadb105-server -y
-pip install mysql-connector-python -y
+pip install mysql-connector-python 
