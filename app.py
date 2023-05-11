2050314691eb62f26bfffb5e50db1f224ffa1149
kalpana <kalpanaironbanda@gmail.com>
2023-05-11 16:08:15 +0530

    modified the code

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import mysql.connector
 app = Flask(__name__)
 @app.route('/')
 def index():
-    cnx = mysql.connector.connect(user='thanshi', password='thanshi123',
-                                   host='collage.ct3ipxrkwhfs.ap-south-1.rds.amazonaws.com',
-                                   database='collage')
+    cnx = mysql.connector.connect(user='give username', password='give the password ',
+                                   host='give the endpoint of database',
+                                   database='give the name of the database')
     cursor = cnx.cursor()
-    query = "SELECT * FROM <tablename>"
+    query = "SELECT * FROM <Ur-Tablename>"
     cursor.execute(query)
     rows = cursor.fetchall()
     cnx.close()
-    return render_template('table.html', rows=rows)
+    return render_template('html file name', rows=rows)
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=80,debug=True)
 
